8fa8672f3825c1a142c675f52ff177d77bc0bf4e
Yoshiya Noda <ny1425.0106@gmail.com>
2020-12-27 00:11:26 +0900

    api疎通確認

--- src/views/HpCreate.vue
+++ src/views/HpCreate.vue
@@ -4,12 +4,24 @@
       <p>Let's make it through to the end.</p>
       <p>Hurry moderately.</p>
       <p>We can do anything.</p>
+      <p>{{msg}}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+    data: function() {
+        return {
+            msg: 'msg'
+        };
+    },
+    mounted: function() {
+        const url = "https://www.yoshiyasakugawa.com/api/fetch";
+        axios.get(url).then(res => {
+           this.msg = res.data;
+        });
+    }
 }
 </script>
 
