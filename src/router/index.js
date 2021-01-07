b7fdfcc27184039d913b906fcb1bdef26002ad51
Yoshiya Noda <ny1425.0106@gmail.com>
2021-01-07 23:01:12 +0900

    sample

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Sample from '../views/Sample.vue'
 import HpCreate from '../views/HpCreate.vue'
 
 Vue.use(VueRouter)
@@ -15,6 +16,11 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/sample',
+    name: 'Sample',
+    component: Sample
   },
   {
     path: '/about',
