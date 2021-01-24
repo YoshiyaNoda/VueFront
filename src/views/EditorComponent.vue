a7985cbaf58a1688f67c769817cb355f2c4b5f9d
Yoshiya Noda <ny1425.0106@gmail.com>
2021-01-25 03:42:57 +0900

    雛形作成

---
+++ src/views/EditorComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <!-- articleData.typeに応じて見た目を変える -->
+    <p>{{ articleData }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    articleData: new Object()
+  },
+  methods: {
+    updateData: function() {
+      this.$parent.articleData[this.articleData.order] = this.articleData;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HpCreate.vue
+++ src/views/HpCreate.vue
@@ -1,28 +1,86 @@
 <template>
   <div class="wrapper">
-		<h1>Hello</h1>
+		<!-- <h1>Hello</h1>
 		<p>Let's make it through to the end.</p>
 		<p>Hurry moderately.</p>
-		<p>We can do anything.</p>
-		<p>{{test}}</p>
+		<p>We can do anything.</p> -->
+		<p>{{articleData}}</p>
+		<div class="iterContainer" @mouseleave="displayTypeSelect(-1)">
+			<div class="typeSelectContainer" v-show="displayedTypeSelect === 0" >
+				<div class="typeSelect">
+					<div class="typeSelectBtnContainer">
+						<button @click="addElement(0, 'heading')">普通の見出し</button>
+					</div>
+				</div>
+			</div>
+			<button class="btn btn-primary" @mouseover="displayTypeSelect(0)">追加</button>
+		</div>
+		<div class="iterContainer" @mouseleave="displayTypeSelect(-1)" v-for="(d, idx) in articleData" :key="'container'+idx">
+			<div class="dataEditorContainer">
+				<editor-component :article-data="d" />
+			</div>
+			<div class="typeSelectContainer" v-show="displayedTypeSelect === idx+1" >
+				<div class="typeSelect">
+					<div class="typeSelectBtnContainer">
+						<button @click="addElement(idx+1, 'heading')">普通の見出し</button>
+					</div>
+				</div>
+			</div>
+			<button class="btn btn-primary" @mouseover="displayTypeSelect(idx+1)">追加</button>
+		</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ArticleComponent from './ArticleComponent.js';
+import EditorComponent from './EditorComponent.vue';
+
 export default {
+	components: {
+		EditorComponent
+	},
 	data: function() {
 		return {
-			articleData: new Object(),
-			test: ''
+			articleData: [],
+			test: '',
+			displayedTypeSelect: -1
 		};
 	},
 	mounted: async function() {
 		// id=1のデータを取り出す
 		const url = "http://localhost:8000/api/fetch_article_data/1";
 		await axios.get(url).then(res => {
-			this.test = res.data;
+			this.test = res.data.msg;
+			if(res.data.status === 0) {
+				// alert('データが存在しません');
+			} else if(res.data.status === 1) {
+				this.articleData = JSON.parse(res.data.data);
+			}
 		});
+	},
+	methods: {
+		displayTypeSelect: function(idx) {
+			this.displayedTypeSelect = idx;
+		},
+		addElement: function(order, type) {
+			// orderによって、他の要素のorderを増加させる必要がある。
+			this.articleData = this.articleData.map(d => {
+				if(d.order >= order) {
+					d.order += 1;
+					return d;
+				}
+				else return d;
+			});
+			// 適切な場所にデータを挿入する
+			this.articleData.splice(order, 0, {
+				order: order,
+				type: type,
+				option: 'normal',
+				// dataはdeepcopyで初期値を設定
+				data: ArticleComponent[type].normal.data
+			})
+		}
 	}
 }
 </script>
