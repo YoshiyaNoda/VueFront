bff6cdf772efb92c9c420ede4161a8016594505c
Yoshiya Noda <ny1425.0106@gmail.com>
2021-01-25 06:15:24 +0900

    プレビュー作成

--- src/views/HpCreate.vue
+++ src/views/HpCreate.vue
@@ -1,32 +1,43 @@
 <template>
-  <div class="wrapper">
+  <div class="container">
 		<!-- <h1>Hello</h1>
 		<p>Let's make it through to the end.</p>
 		<p>Hurry moderately.</p>
 		<p>We can do anything.</p> -->
-		<p>{{articleData}}</p>
-		<div class="iterContainer" @mouseleave="displayTypeSelect(-1)">
-			<div class="typeSelectContainer" v-show="displayedTypeSelect === 0" >
-				<div class="typeSelect">
-					<div class="typeSelectBtnContainer">
-						<button @click="addElement(0, 'heading')">普通の見出し</button>
-					</div>
-				</div>
+		<div class="tabArea">
+			<div class="tabBtnContainer">
+				<button class="btn" @click="selectTab(1)">編集画面</button>
+				<button class="btn" @click="selectTab(2)">Preview</button>
 			</div>
-			<button class="btn btn-primary" @mouseover="displayTypeSelect(0)">追加</button>
 		</div>
-		<div class="iterContainer" @mouseleave="displayTypeSelect(-1)" v-for="(d, idx) in articleData" :key="'container'+idx">
-			<div class="dataEditorContainer">
-				<editor-component :article-data="d" />
-			</div>
-			<div class="typeSelectContainer" v-show="displayedTypeSelect === idx+1" >
-				<div class="typeSelect">
-					<div class="typeSelectBtnContainer">
-						<button @click="addElement(idx+1, 'heading')">普通の見出し</button>
+		<div class="editArea" v-show="selectedTab === 1">
+			<p>{{articleData}}</p>
+			<div class="iterContainer" @mouseleave="displayTypeSelect(-1)">
+				<div class="typeSelectContainer" v-show="displayedTypeSelect === 0" >
+					<div class="typeSelect">
+						<div class="typeSelectBtnContainer">
+							<button @click="addElement(0, 'heading')">普通の見出し</button>
+						</div>
 					</div>
 				</div>
+				<button class="btn btn-primary" @mouseover="displayTypeSelect(0)">追加</button>
 			</div>
-			<button class="btn btn-primary" @mouseover="displayTypeSelect(idx+1)">追加</button>
+			<div class="iterContainer" @mouseleave="displayTypeSelect(-1)" v-for="(d, idx) in articleData" :key="'container'+idx">
+				<div class="dataEditorContainer">
+					<editor-component :article-data="d" />
+				</div>
+				<div class="typeSelectContainer" v-show="displayedTypeSelect === idx+1" >
+					<div class="typeSelect">
+						<div class="typeSelectBtnContainer">
+							<button @click="addElement(idx+1, 'heading')">普通の見出し</button>
+						</div>
+					</div>
+				</div>
+				<button class="btn btn-primary" @mouseover="displayTypeSelect(idx+1)">追加</button>
+			</div>
+		</div>
+		<div class="previewArea" v-show="selectedTab === 2">
+			<preview :article-data="articleData"/>
 		</div>
   </div>
 </template>
@@ -36,16 +47,20 @@ import axios from 'axios';
 import ArticleComponent from './ArticleComponent.js';
 import EditorComponent from './EditorComponent.vue';
 import _ from 'lodash'; //ちゃんとは調べてないけどDeep Copyした方が良さそうなので、そのためにlodashを使う
+import Preview from './Preview.vue';
+
 
 export default {
 	components: {
-		EditorComponent
+		EditorComponent,
+		Preview
 	},
 	data: function() {
 		return {
 			articleData: [],
 			test: '',
-			displayedTypeSelect: -1
+			displayedTypeSelect: -1,
+			selectedTab: 1
 		};
 	},
 	mounted: async function() {
@@ -61,6 +76,9 @@ export default {
 		});
 	},
 	methods: {
+		selectTab: function(num) {
+			this.selectedTab = num;
+		},
 		displayTypeSelect: function(idx) {
 			this.displayedTypeSelect = idx;
 		},
@@ -87,5 +105,7 @@ export default {
 </script>
 
 <style lang=scss scoped>
-
+.tabBtnContainer {
+	display: flex;
+}
 </style>
--- src/views/editors/HeadingEditor.vue
+++ src/views/editors/HeadingEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <!-- これを変更すると親が持つデータも変更される -->
-    <p>order: {{d.order}}</p>
+    <!-- <p>order: {{d.order}}</p> -->
     <textarea type="text" v-model="d.data.content"></textarea>
   </div>
 </template>
